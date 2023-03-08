25e8b2a016d1c702f13b93fefbf5e6327a3509e6
Argent <jrargent42@gmail.com>
2023-03-08 13:28:38 -0500

    Add actor model, encounter model, and threat level enum

---
+++ AppOfManyThings/AOMT.Core/Enums/ThreatLevel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOMT.Core.Enums
+{
+    public enum ThreatLevel
+    {
+        Trivial = 10,
+        Low = 15,
+        Moderate = 20,
+        Severe = 30,
+        Extreme = 40
+
+    }
+}
--- AppOfManyThings/AOMT.Core/Models/Actor.cs
+++ AppOfManyThings/AOMT.Core/Models/Actor.cs
@@ -1,7 +1,40 @@
 ﻿namespace AOMT.Core.Models
 {
-    public class Actor
+    public class ActorModel // properties are 
     {
+        public ActorModel(string id, string name, string aonUrl, string family, 
+            int level, string alignment, string type, string size, string source, 
+            string sourceType, string rarity, string[] traits, string description)
+        {
+            Id = id;
+            this.Name = name;
+            this.AonUrl = aonUrl;
+            this.Family = family;
+            this.Level = level;
+            this.Alignment = alignment;
+            this.Type = type;
+            this.Size = size;
+            this.Source = source;
+            this.SourceType = sourceType;
+            this.Rarity = rarity;
+            this.Traits = traits;
+            this.Description = description;
+        }
+
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string AonUrl { get; set; }
+        public string Family { get; set; }
+        public int Level { get; set; }
+        public string Alignment { get; set; }
+        public string Type { get; set; }
+        public string Size { get; set; }
+        public string Source { get; set; }
+        public string SourceType { get; set; }
+        public string Rarity { get; set; }
+        public string[] Traits { get; set; }
+        public string Description { get; set; }
+
 
     }
 }
